2b79b9ace6903442c50e3bfc3dea1b73cf2bf67b
Alexandr <mr-hornet@mail.ru>
2023-03-30 19:12:30 +1000

    done

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Main {
@@ -20,44 +21,38 @@ public class Main {
         System.out.println("Задача 3");
         System.out.println("Введите расстояние от офиса до адреса доставки карты:");
         int deliveryDistance = scanner.nextInt();
-        if (task3(deliveryDistance) == 0) {
+        int a = task3(deliveryDistance);
+        if (a == 0) {
             System.out.println("Доставки нет");
         } else {
-            System.out.println("Потребуется " + task3(deliveryDistance) + " дней для доставки");
+            System.out.println("Потребуется " + a + " дней для доставки");
         }
     }
 
     public static void task1(int year) {
 
-        if (year % 4 == 0 && year % 100 != 0) {
-            System.out.println("Этот год высокосный");
-        } else if (year % 400 == 0) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.println("Этот год высокосный");
         } else {
             System.out.println("Этот год не высокосный");
         }
     }
 
-    public static void task2 (int clientOS, int clientDeviceYear) {
-
-        while (true) {
-            if (clientOS == 0) {
-                System.out.println("Установите версию приложения для iOS по ссылке");
-                if (clientDeviceYear < 2015) {
-                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-                }
-                break;
-            } else if (clientOS == 1) {
-                System.out.println("Установите версию приложения для Android по ссылке");
-                if (clientDeviceYear < 2015) {
-                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
-                }
-                break;
-            } else {
-                throw new RuntimeException("Такой ОС не существует");
+    public static void task2(int clientOS, int clientDeviceYear) {
+        int currentYear = LocalDate.now().getYear();
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+            if (clientDeviceYear < currentYear) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
             }
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+            if (clientDeviceYear < currentYear) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+        } else {
+            throw new RuntimeException("Такой ОС не существует");
         }
-
     }
 
     public static int task3(int deliveryDistance) {
